c887f2e0819b187b8e2d7217a267aa3a20ae8d37
David A. van Leeuwen <david.vanleeuwen@gmail.com>
2015-12-20 14:45:57 +0100

    Add more info about prompt texts

--- bin/answers.py
+++ bin/answers.py
@@ -23,11 +23,18 @@ def string(x):
         return str(x)
 
 def prompt(task):
-    sp_text = text.get(task.question_recording_id, None)
-    if sp_text:
-        return "/".join(sp_text)
+    prompttext = text.get(task.question_recording_id)
+    if prompttext:
+        return "/".join(prompttext)
     else:
         return None
+
+def textinfo(rid):
+    tt = texttype.get(rid)
+    if tt:
+        return [tt.key, tt.type]
+    else:
+        return [None, None]
 
 logging.basicConfig(level=logging.INFO)
 #logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
@@ -35,8 +42,9 @@ logging.basicConfig(level=logging.INFO)
 ## find text with recording_id
 text = dict()
 speaker = dict()
-qq = session.query(Tasks, Texts).join(TaskText).filter(TaskText.text_id == Texts.id).order_by(Texts.id)
-for ta, te in qq:
+texttype= dict()
+qq = session.query(Tasks, Texts, TextGroups).join(TaskText).filter(TaskText.text_id == Texts.id, TextGroups.id == Texts.text_group_id).order_by(Texts.id)
+for ta, te, tg in qq:
     if not ta.recording_id:
         continue
     addto(text, ta.recording_id, norm(te.text))
@@ -45,26 +53,29 @@ for ta, te in qq:
             print "Inconsistent", speaker[ta.recording_id], ta.profile_id
     else:
         speaker[ta.recording_id] = ta.profile_id
+        texttype[ta.recording_id] = tg
 logging.info("Number of recordings: %d", len(text))
 
 ## main loop over questions
 qq = session.query(Questions).join(QuestionGroups, Components).filter(QuestionGroups.name.like("Vragenlijst%"))
 
-print ", ".join([string(x) for x in ["qid", "qtype", "lid", "sid", "value", "prompt"]])
+print ", ".join([string(x) for x in ["qid", "atype", "qlist", "qtype", "lid", "sid", "value", "prompt"]])
 
 for q in qq.filter(Components.type == "slider"):
     logging.info("Question %d: %s", q.id, q.question)
     qid = "q%02d" % q.id
     aq = session.query(Tasks, Answers).filter(Tasks.question_id == q.id, Answers.id == Tasks.answer_id)
     for t, a in aq.order_by(Answers.created_at):
-        print ",".join([string(x) for x in [qid, q.components.type, t.profile_id, speaker.get(t.question_recording_id), a.answer_numeric, prompt(t)]])
+        rid = t.question_recording_id
+        print ",".join([string(x) for x in [qid, q.components.type] + textinfo(rid) + [t.profile_id, speaker.get(rid), a.answer_numeric, prompt(t)]])
 
 for q in qq.filter(or_(Components.type == "yesno", Components.type == "option")):
     logging.info("Question %d: %s", q.id, q.question)
     qid = "q%02d" % q.id
     aq = session.query(Tasks, Options).join(Answers, AnswerOption).filter(Tasks.question_id == q.id, AnswerOption.option_id == Options.id)
     for t, o in aq.order_by(Answers.created_at):
-        print ",".join([string(x) for x in [qid, q.components.type, t.profile_id, speaker.get(t.question_recording_id), o.value, prompt(t)]])
+        rid = t.question_recording_id
+        print ",".join([string(x) for x in [qid, q.components.type] + textinfo(rid) + [t.profile_id, speaker.get(rid), o.value, prompt(t)]])
 
 for q in qq.filter(Components.type == "location"):
     logging.info("Question %d: %s", q.id, q.question)
@@ -72,5 +83,6 @@ for q in qq.filter(Components.type == "location"):
     aq = session.query(Tasks, Locations).join(Answers, AnswerLocation).filter(Tasks.question_id == q.id, AnswerLocation.location_id == Locations.id)
     for t, l in aq:
         loc = "%8.6f/%8.6f/%d" % (l.longitude, l.latitude, l.mapzoom)
-        print ",".join([string(x) for x in [qid, q.components.type, t.profile_id, speaker.get(t.question_recording_id), loc, prompt(t)]])
+        rid = t.question_recording_id
+        print ",".join([string(x) for x in [qid, q.components.type] + textinfo(rid) + [t.profile_id, speaker.get(rid), loc, prompt(t)]])
 
